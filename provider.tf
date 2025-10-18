e0c756994f594ea91e2b73d775c61df627cceba4
Hemachandran5080 <58474445+Hemachandran5080@users.noreply.github.com>
2025-10-18 18:57:03 +0530

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,8 @@
+// ...existing code...
+provider "google" {
+  project     = "qwiklabs-gcp-02-f9066f830681"
+  region      = "us-central1"
+  credentials = file("D:/Dataengineeringprojects/Uber-data-analytics/qwiklabs-gcp-02-f9066f830681-d3e374ad6dd8.json")  # set your absolute path
+}
+
+
